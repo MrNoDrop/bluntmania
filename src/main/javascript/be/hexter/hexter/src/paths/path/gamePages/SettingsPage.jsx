531e792856746d2ffae198820ca169ebefa9b610
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2024-01-20 19:23:20 +0100

    Created a settings page.

---
+++ src/main/javascript/be/hexter/hexter/src/paths/path/gamePages/SettingsPage.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { push } from "redux-first-routing";
+import Title from "../../../gfx/mainMenuPage/title";
+
+const mapStateToProps = ({ state }) => ({
+  ctx: state.context2D,
+  updateTick: state.updateTick,
+  windowSize: state.window.inner,
+});
+
+const mapDispatchToProps = (dispatch) => ({});
+
+function SettingsPage({ ctx, updateTick, windowSize }) {
+  const navigateTo = useNavigate();
+  useEffect(() => {
+    if (!ctx) {
+      navigateTo("/");
+    } else {
+      ctx.clearRect(0, 0, windowSize.width, windowSize.height);
+    }
+  }, [ctx, navigateTo, updateTick, windowSize]);
+  return (
+    <>
+      <Title />
+    </>
+  );
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SettingsPage);
