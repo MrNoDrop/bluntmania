036fe04e19c0bf228a4e4aacc094a97d7a21fb6a
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2024-01-20 18:52:58 +0100

    Made useHowl return the sound source.

--- src/main/javascript/be/hexter/hexter/src/hooks/useHowl.js
+++ src/main/javascript/be/hexter/hexter/src/hooks/useHowl.js
@@ -1,17 +1,20 @@
 import { Howl } from "howler";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const useHowl = (src, loop) => {
+  const [sound_, setSound] = useState(undefined);
   useEffect(() => {
     var sound = new Howl({
       src: ["http://localhost:8080/rsc/sounds/background/happy.wav"],
       autoplay: true,
       loop,
     });
+    setSound(sound);
     return () => {
       sound.stop();
     };
   }, [src, loop]);
+  return sound_;
 };
 
 export default useHowl;
