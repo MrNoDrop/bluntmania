7fd02aae2528eb7768a19c0c1f8550ad43ce1fda
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2024-01-25 19:01:38 +0100

    Added player component

--- src/main/javascript/be/hexter/hexter/src/paths/path/gamePages/chooseLevel/level1.jsx
+++ src/main/javascript/be/hexter/hexter/src/paths/path/gamePages/chooseLevel/level1.jsx
@@ -1,16 +1,25 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import Player from "../../../../gfx/environment/player";
 
 const mapStateToProps = ({ state }) => ({
   ctx: state.context2D,
   updateTick: state.updateTick,
   windowSize: state.window.inner,
+  environmentOffsetX: state.environmentOffsetX,
+  environmentOffsetY: state.environmentOffsetY,
 });
 
 const mapDispatchToProps = (dispatch) => ({});
 
-function Level1Page({ ctx, updateTick, windowSize }) {
+function Level1Page({
+  ctx,
+  updateTick,
+  windowSize,
+  environmentOffsetX,
+  environmentOffsetY,
+}) {
   const navigateTo = useNavigate();
   window.navigateTo = navigateTo;
   useEffect(() => {
@@ -20,7 +29,11 @@ function Level1Page({ ctx, updateTick, windowSize }) {
       ctx.clearRect(0, 0, windowSize.width, windowSize.height);
     }
   }, [ctx, navigateTo, updateTick, windowSize]);
-  return <></>;
+  return (
+    <>
+      <Player />
+    </>
+  );
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Level1Page);
