989f84d76904fe66bcdc94c622310ca30236611c
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2024-01-25 21:26:32 +0100

    Removed

--- src/main/javascript/be/hexter/hexter/src/store/actions/set/level/size.js
+++
@@ -1,7 +0,0 @@
-import { SET_LEVEL_SIZE } from "../../types";
-
-const setLevelSize = (currentState, width, height) => ({
-  type: SET_LEVEL_SIZE,
-  payload: { level: { ...currentState.level, width, height } },
-});
-export default setLevelSize;
