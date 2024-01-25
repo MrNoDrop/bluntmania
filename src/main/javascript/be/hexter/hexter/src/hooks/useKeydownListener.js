bad5902f24869896a981e3b221453438552ad11b
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2024-01-25 19:03:55 +0100

    Created keydown listener hook

---
+++ src/main/javascript/be/hexter/hexter/src/hooks/useKeydownListener.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+const useKeydownListener = () => {
+  const [keys, setKeys] = useState({});
+  useEffect(() => {
+    window.onkeydown = window.onkeyup = function (e) {
+      const keysCopy = { ...keys };
+      keysCopy[e.key] = e.type === "keydown";
+      setKeys(keysCopy);
+    };
+    return () => {
+      window.onkeydown = window.onkeyup = undefined;
+    };
+  }, [keys]);
+  return keys;
+};
+
+export default useKeydownListener;
